962fb8eed17f7ad1d14f412bd077a2ae84d5e8d8
aecio <aecio.jorge@gmail.com>
2020-01-02 18:00:46 -0400

    add transformations

---
+++ Assets/Transformations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformations : MonoBehaviour
+{
+    public GameObject[] points;
+    public Vector3 angle;
+    public Vector3 translation;
+    public Vector3 scaling;
+    public Vector3 shear;
+    public GameObject centre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 c = new Vector3(centre.transform.position.x,
+                                centre.transform.position.y,
+                                centre.transform.position.z);
+
+        angle = angle * Mathf.Deg2Rad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
